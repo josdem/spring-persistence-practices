08bbfa6d674a13979d24a9a30007382fb0739944
neodevelop <neodevelop@gmail.com>
2014-02-14 18:03:06 -0600

    agregando practica de DAO

---
+++ src/main/java/com/makingdevs/practica4/UserStoryDaoJdbcImpl.java
@@ -0,0 +1,85 @@
+package com.makingdevs.practica4;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.makingdevs.dao.UserStoryDao;
+import com.makingdevs.model.Project;
+import com.makingdevs.model.UserStory;
+
+@Repository
+public class UserStoryDaoJdbcImpl extends JdbcDaoSupport implements UserStoryDao {
+  
+  @Autowired
+  public UserStoryDaoJdbcImpl(DataSource dataSource){
+    super.setDataSource(dataSource);
+  }
+
+  private static String sqlQuery = "SELECT * FROM user_story";
+
+  @Override
+  public Long create(UserStory newInstance) {
+    String sqlInsert = "INSERT INTO user_story(DATE_CREATED,DESCRIPTION,EFFORT,LAST_UPDATED,PRIORITY,PROJECT_ID) "
+        + "VALUES(?,?,?,?,?,?);";
+    getJdbcTemplate().update(sqlInsert, new Date(), newInstance.getDescription(), newInstance.getEffort(), new Date(),
+        newInstance.getPriority(), newInstance.getProject().getId());
+    Long userStoryId = getJdbcTemplate().queryForObject(
+        "SELECT id FROM user_story WHERE DESCRIPTION = ? AND PROJECT_ID = ?",
+        new Object[] { newInstance.getDescription(), newInstance.getProject().getId() }, Long.class);
+    return userStoryId;
+  }
+
+  @Override
+  public UserStory read(Long id) {
+    return getJdbcTemplate().queryForObject(sqlQuery + " WHERE ID = ?", new Object[] { id }, new UserStoryMapper());
+  }
+
+  @Override
+  public void update(UserStory transientObject) {
+    String sqlUpdate = "UPDATE user_story SET DESCRIPTION = ?,EFFORT = ?,LAST_UPDATED = ?,PRIORITY = ?,PROJECT_ID = ? WHERE ID = ?";
+    getJdbcTemplate().update(sqlUpdate, transientObject.getDescription(), transientObject.getEffort(), new Date(),
+        transientObject.getPriority(), transientObject.getProject().getId(), transientObject.getId());
+  }
+
+  @Override
+  public void delete(UserStory persistentObject) {
+    String sqlDelete = "DELETE FROM user_story WHERE ID = ?";
+    getJdbcTemplate().update(sqlDelete, persistentObject.getId());
+  }
+
+  @Override
+  public List<UserStory> findAll() {
+    return getJdbcTemplate().query(sqlQuery, new UserStoryMapper());
+  }
+
+  @Override
+  public int countAll() {
+    String countQuery = "SELECT count(*) FROM user_story";
+    return getJdbcTemplate().queryForObject(countQuery, Integer.class);
+  }
+
+  @Override
+  public List<UserStory> findAllByEffortBetween(Integer lowValue, Integer maxValue) {
+    String findAllByEffortBetweenSql = sqlQuery + " WHERE EFFORT BETWEEN ? AND ?";
+    return getJdbcTemplate().query(findAllByEffortBetweenSql, new Object[] { lowValue, maxValue }, new UserStoryMapper());
+  }
+
+  @Override
+  public List<UserStory> findAllByPriorityBetween(Integer lowValue, Integer maxValue) {
+    String findAllByPriorityBetweenSql = sqlQuery + " WHERE PRIORITY BETWEEN ? AND ?";
+    return getJdbcTemplate().query(findAllByPriorityBetweenSql, new Object[] { lowValue, maxValue }, new UserStoryMapper());
+  }
+
+  @Override
+  public List<UserStory> findAllByProject(Project project) {
+    String findByProjectIdSql = "SELECT * FROM user_story us INNER JOIN project p ON p.id = us.project_id WHERE us.project_id = ?";
+    return getJdbcTemplate().query(findByProjectIdSql, new Object[] { project.getId() }, new UserStoryMapper());
+  }
+
+}
---
+++ src/test/java/com/makingdevs/practica4/UserStoryDaoJdbcImplTests.java
@@ -0,0 +1,91 @@
+package com.makingdevs.practica4;
+
+import static org.springframework.util.Assert.isTrue;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.makingdevs.dao.UserStoryDao;
+import com.makingdevs.model.Project;
+import com.makingdevs.model.UserStory;
+import static org.springframework.util.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "FirstDaoAppCtx.xml", "../practica1/DataSourceWithNamespace.xml" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UserStoryDaoJdbcImplTests {
+
+  @Autowired
+  UserStoryDao userStoryDao;
+
+  private static long userStoryId;
+
+  @Test
+  public void test0FindAll() {
+    List<UserStory> userStories = userStoryDao.findAll();
+    notEmpty(userStories);
+  }
+
+  @Test
+  public void test1CreateUserStory() {
+    UserStory us = new UserStory();
+    us.setEffort(3);
+    us.setPriority(1);
+    us.setDescription("As user... I want... In order to...");
+    Project p = new Project();
+    p.setId(1L);
+    us.setProject(p);
+    userStoryId = userStoryDao.create(us);
+    isTrue(userStoryId > 0);
+  }
+
+  @Test
+  public void test2QueryUserStory() {
+    UserStory userStory = userStoryDao.read(userStoryId);
+    isTrue(3 == userStory.getEffort());
+    isTrue(1 == userStory.getPriority());
+    Assert.assertEquals("As user... I want... In order to...", userStory.getDescription());
+    Assert.assertNotNull(userStory.getProject());
+    isTrue(userStory.getProject().getId() == 1);
+  }
+
+  @Test
+  public void test3UpdateUserStory() {
+    UserStory userStory = userStoryDao.read(userStoryId);
+    String oldDescription = userStory.getDescription();
+    userStory.setDescription("As admin... I wish... Because...");
+    userStory.setEffort(5);
+    userStory.setPriority(4);
+    userStoryDao.update(userStory);
+    UserStory userStoryUpdated = userStoryDao.read(userStoryId);
+    isTrue(5 == userStoryUpdated.getEffort());
+    isTrue(4 == userStoryUpdated.getPriority());
+    Assert.assertEquals("As admin... I wish... Because...", userStoryUpdated.getDescription());
+    Assert.assertNotEquals(oldDescription, userStoryUpdated.getDescription());
+  }
+
+  @Test(expected = DataAccessException.class)
+  public void test4DeleteUserStory() {
+    UserStory userStory = userStoryDao.read(userStoryId);
+    userStoryDao.delete(userStory);
+    userStoryDao.read(userStoryId);
+  }
+
+  @Test
+  public void test5FindByEffort() {
+    List<UserStory> userStories = userStoryDao.findAllByEffortBetween(1, 3);
+    for (UserStory us : userStories) {
+      isTrue(us.getEffort() >= 1 && us.getEffort() <= 3);
+    }
+  }
+
+}
